18f7f5a92b32ee36f66e93a5d33dfeaed2873629
russenreaktor <m.kupriyanov@gmail.com>
2013-07-04 12:54:32 +0200

    updated template to manifest_version:2 added content_security_policy

--- extension/js/analytics.js
+++ extension/js/analytics.js
@@ -2,8 +2,9 @@
   _gaq.push(['_setAccount', '<%PUT_YOUR_UA_IS_HERE%>']);
   //_gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
